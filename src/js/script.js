3c7e9a23778c562906b43a14fc35c7387892c878
Max <maximpalaguta@gmail.com>
2021-03-10 00:02:57 +0200

    added adoptation for 320-767

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,27 @@
 $(document).ready(function () {
+    window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu__item'),
+        hamburger = document.querySelector('.hamburger');
+    
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('menu_active');
+            });
+        });
+    });
+
     $('.phones__slider').slick({
         arrows: false,
         slidesToShow: 1,
         speed: 800,
+        zIndex: 1,
     });
 
     new WOW().init();
